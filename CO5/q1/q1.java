1ab36204bc2c9f6bec3c2b98d3742d0b0f43db0c
Hamna K A <88711212+HamnaKA@users.noreply.github.com>
2022-07-11 17:06:22 +0530

    Add files via upload

---
+++ CO5/q1/q1.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.applet.*;
+public class q1 extends Applet
+{
+   public void paint(Graphics g)
+   {
+      g.drawRect(374,266,314,270);
+      g.drawRect(483,414,85,121);
+      g.drawOval(549,68,81,46);
+      g.drawLine(374,266,547,149);
+      g.drawLine(547,149,688,262);
+      g.drawLine(547,149,547,68);
+      
+   }
+}
