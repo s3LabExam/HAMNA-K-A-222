4ccc2c6290cfb4cfe4446a98a880d6e434543596
Hamna K A <88711212+HamnaKA@users.noreply.github.com>
2022-07-14 14:41:07 +0530

    Update q9.java

--- CO4/q9.java
+++ CO4/q9.java
@@ -1,4 +1,38 @@
 import java.util.*;
+class BubbleSort<T extends Comparable<? super T>> {
+
+       T array[];
+	  BubbleSort(T array[])
+       {
+    	   this.array=array;;
+       }
+	public void display() {
+		  for(int i=0;i<array.length;i++)
+		  {
+			  System.out.println(array[i]);
+		  }
+	
+	   }
+	public void sort() {
+	     for(int i=0;i<array.length-1;i++)
+	     {
+	    	 for(int j=0;j<(array.length)-1;j++)
+	    	 {
+	    		 if(array[j].compareTo(array[j+1])>0){
+	    		 {
+	    			T temp=array[j];
+	    			array[j]=array[j+1];
+	    			array[j+1]=temp;
+	    			
+	    			
+	    		 }
+	    	 }
+	     }
+		
+	}
+	}
+}
+
 public class q9 {
 
 	public static void main(String[] args) {
